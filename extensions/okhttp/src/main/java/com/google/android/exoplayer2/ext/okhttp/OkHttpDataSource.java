b5d6f7d448c0cead0bbb13c567b1f42375ef2276
fgl27 <fglfgl27@gmail.com>
2020-03-11 16:27:28 -0300

    Revert "Update com.squareup.okhttp3 to 4.0.1 fix deprecated"
    This reverts commit 13c3574770b7d8a62825e61d368db254ffba46e5.

--- extensions/okhttp/src/main/java/com/google/android/exoplayer2/ext/okhttp/OkHttpDataSource.java
+++ extensions/okhttp/src/main/java/com/google/android/exoplayer2/ext/okhttp/OkHttpDataSource.java
@@ -363,10 +363,10 @@ public class OkHttpDataSource extends BaseDataSource implements HttpDataSource {
 
     RequestBody requestBody = null;
     if (dataSpec.httpBody != null) {
-      requestBody = RequestBody.create(dataSpec.httpBody, null);
+      requestBody = RequestBody.create(null, dataSpec.httpBody);
     } else if (dataSpec.httpMethod == DataSpec.HTTP_METHOD_POST) {
       // OkHttp requires a non-null body for POST requests.
-      requestBody = RequestBody.create(Util.EMPTY_BYTE_ARRAY, null);
+      requestBody = RequestBody.create(null, Util.EMPTY_BYTE_ARRAY);
     }
     builder.method(dataSpec.getHttpMethodString(), requestBody);
     return builder.build();
